341ff48bc73f8d3f2ed184e6633afb59056e0e69
Eduardo Henn <59212692+eduhenn5@users.noreply.github.com>
2023-12-24 12:08:33 -0300

    Add files via upload

---
+++ ListaDeCompras.py
@@ -0,0 +1,13 @@
+class ListaDeCompras:
+    def __init__(self):
+        self.itens = []
+
+    def adicionar_item(self, item):
+        self.itens.append(item)
+
+    def remover_item(self, item):
+        if item in self.itens:
+            self.itens.remove(item)
+
+    def listar_itens(self):
+        return self.itens
